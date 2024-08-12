c2db282dc45157772825622fbd539abe06682c5b
JOSE RODRIGUEZ <josev01@gmail.com>
2024-08-12 09:33:05 -0300

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -108,7 +108,7 @@ defmodule NervesSystemEffioReTerminalRpi4.MixProject do
       "fwup-ops.conf",
       "fwup.conf",
       "LICENSE",
-      "linux-6.6.defconfig",
+      "linux-6.1.defconfig",
       "mix.exs",
       "nerves_defconfig",
       "post-build.sh",
