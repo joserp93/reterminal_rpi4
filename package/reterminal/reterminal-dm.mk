70afd3a724777cbc7d98affd58290d55186751f3
Jose Rodriguez <josev01@gmail.com>
2024-08-19 11:33:10 -0300

    add rtc

--- package/reterminal/reterminal-dm.mk
+++ package/reterminal/reterminal-dm.mk
@@ -11,7 +11,7 @@ RETERMINAL_DM_VERSION = 0.0.1
 RETERMINAL_DM_DEPENDENCIES += host-dtc
 
 define RETERMINAL_DM_BUILD_CMDS
-	cp $(NERVES_DEFCONFIG_DIR)/package/reterminal-dm/*.dts* $(@D)
+	cp $(NERVES_DEFCONFIG_DIR)/package/reterminal/*.dts* $(@D)
         for filename in $(@D)/*.dts; do \
             $(CPP) -I$(@D) -I $(LINUX_SRCDIR)include -I $(LINUX_SRCDIR)arch -nostdinc -undef -D__DTS__ -x assembler-with-cpp $$filename | \
               $(HOST_DIR)/usr/bin/dtc -Wno-unit_address_vs_reg -@ -I dts -O dtb -b 0 -o $${filename%.dts}.dtbo || exit 1; \
