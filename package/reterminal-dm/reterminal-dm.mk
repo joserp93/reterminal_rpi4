a29a284a6f79c23653487a38ed2830f91510cf6f
Jose Rodriguez <josev01@gmail.com>
2024-08-20 10:33:42 -0300

    reterminal overlays

--- package/reterminal-dm/reterminal-dm.mk
+++ package/reterminal-dm/reterminal-dm.mk
@@ -1,24 +1,24 @@
 #############################################################
 #
-# reterminal
+# reterminal-dm
 #
 #############################################################
 
 # Remember to bump the version when anything changes in this
 # directory.
-RETERMINAL_SOURCE =
-RETERMINAL_VERSION = 0.0.1
-RETERMINAL_DEPENDENCIES += host-dtc
+RETERMINAL_DM_SOURCE =
+RETERMINAL_DM_VERSION = 0.0.1
+RETERMINAL_DM_DEPENDENCIES += host-dtc
 
-define RETERMINAL_BUILD_CMDS
-	cp $(NERVES_DEFCONFIG_DIR)/package/reterminal/*.dts* $(@D)
+define RETERMINAL_DM_BUILD_CMDS
+	cp $(NERVES_DEFCONFIG_DIR)/package/reterminal-dm/*.dts* $(@D)
         for filename in $(@D)/*.dts; do \
             $(CPP) -I$(@D) -I $(LINUX_SRCDIR)include -I $(LINUX_SRCDIR)arch -nostdinc -undef -D__DTS__ -x assembler-with-cpp $$filename | \
               $(HOST_DIR)/usr/bin/dtc -Wno-unit_address_vs_reg -@ -I dts -O dtb -b 0 -o $${filename%.dts}.dtbo || exit 1; \
         done
 endef
 
-define RETERMINAL_INSTALL_TARGET_CMDS
+define RETERMINAL_DM_INSTALL_TARGET_CMDS
 	echo "$(@D)/*.dtbo -> $(BINARIES_DIR)/rpi-firmware/overlays/"
 	$(INSTALL) -D -m 0644 $(@D)/*.dtbo $(BINARIES_DIR)/rpi-firmware/overlays/
 
